7e3759db1e7a6ff16a33e6fa95360b0132cce44e
Denphum Nakglam <denphum.nakglam@mail.kmutt.ac.th>
2024-03-16 02:38:51 +0700

    [Update] websocket

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./styles/style.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import App from "./App";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store/Store.jsx"; // Assuming you've created your Redux store
 
